3f3378b1e5d1b17abc82c064e29773e8057fe18e
Brion Mario <info@brionmario.com>
2021-08-12 19:08:15 +0530

    chore(exp): update app.jsx

--- technical/how-to-add-a-custom-eslint-configuration-to-a-create-react-app-project/javascript/src/App.js
+++ technical/how-to-add-a-custom-eslint-configuration-to-a-create-react-app-project/javascript/src/App.js
@@ -3,8 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
+    return (<div className="App">
             <header className="App-header">
                 <img src={ logo } className="App-logo" alt="logo" />
                 <p>
